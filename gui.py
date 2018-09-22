9fc7610a800178af11bcbd18f18714f642f57eab
adamjenkins1 <adamjenkins1701@gmail.com>
2018-09-22 14:59:37 -0700

    Adds drop down menu for partitions
    TODO: add check box to show/hide partitions on primary disk

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from tkinter import (Tk, Frame, Button, Entry, Label, StringVar, 
-        TclError, W, NSEW, filedialog, Message)
+        TclError, W, E, NSEW, filedialog, Message, OptionMenu)
 from pathlib import Path
 from bootableDiskCreator import BootableDiskCreator
 import sys
@@ -12,9 +12,13 @@ class GUI(Frame):
     partition = ''
     isoDisplay = object()
     bdc = object()
+    partitionMenu = object()
+    partitions = {}
+    selectedPartition = object()
 
     def __init__(self, parent):
         Frame.__init__(self, parent)
+        self.partitions = {}
         self.bdc = BootableDiskCreator()
         self.parent = parent
         self.iso = ''
@@ -22,14 +26,21 @@ class GUI(Frame):
         self.parent.title('Bootable Disk Creator GUI')
         self.isoDisplay = StringVar()
         self.isoDisplay.set('click "browse" to select the desired ISO image')
+        self.selectedPartition = StringVar()
+        self.selectedPartition.set('')
 
         # create wigits
-        Button(self.parent, text='Browse', command=self.selectISO).grid(row=1, column=1, sticky=W)
-        Entry(self.parent, textvariable=self.isoDisplay, state='readonly', width=65).grid(row=1, column=0, padx=20, sticky=W)
         Message(self.parent, width=300, text=('Here\'s how to use this application:\n1. Select '
-                                              'your ISO image\n2.Select the partition to be used\n'
+                                              'your ISO image\n2. Select the partition to be used\n'
                                               '3. Click "Go" and we\'ll handle the rest')
                                               ).grid(row=0, column=0, padx=20, ipady=20, sticky=W)
+        Button(self.parent, text='Browse', command=self.selectISO).grid(row=1, column=1, sticky=W)
+        Entry(self.parent, textvariable=self.isoDisplay, state='readonly', width=65).grid(row=1, column=0, padx=20, sticky=W)
+
+        Label(self.parent, text='Select partition from drop down menu (you must select an ISO image first)').grid(row=2, column=0, padx=20, pady=(20, 0), sticky=W)
+        self.partitionMenu = OptionMenu(self.parent, self.selectedPartition, '')
+        self.partitionMenu.children['menu'].delete(0, 'end')
+        self.partitionMenu.grid(row=3, column=0, padx=20, sticky=W)
 
 
         # call a dummy dialog with an impossible option to initialize the file
@@ -48,15 +59,27 @@ class GUI(Frame):
                 title = 'Select ISO image', 
                 filetypes = (('ISO files', '*.iso'), ('all files', '*.*')))
 
+        self.partitions = self.getAvailablePartitions()
+        choices = list(self.partitions.keys())
+        self.selectedPartition.set(choices[0])
+        self.partitionMenu = OptionMenu(self.parent, self.selectedPartition, *choices)
+
         if str(self.iso) == '()' or self.iso == '':
             self.iso = 'click "browse" to select the desired ISO image'
+            self.partitionMenu.children['menu'].delete(0, 'end')
+            self.selectedPartition.set('')
 
         self.isoDisplay.set(self.iso)
+        self.partitionMenu.grid(row=3, column=0, padx=20, sticky=W)
 
+        print(self.partitions)
+
+    def getAvailablePartitions(self):
         self.suppressStdout()
         partitions = self.bdc.getAvailablePartitions()
+        partitions = {key:val for (key, val) in partitions.items() if val != '/' and '/boot' not in val}
         self.enableStdout()
-        print(partitions)
+        return partitions
 
     def suppressStdout(self):
         sys.stdout = open(os.devnull, 'w')
