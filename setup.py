34f438b496e0e7395f44bfa9d707df43d6d7dabc
adamjenkins1 <adamjenkins1701@gmail.com>
2018-10-30 00:35:33 -0700

    Changes version to release 1.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
      name='bdc',  
-     version='1.0.0b1',
+     version='1.0.0',
      scripts=['src/bdc/bdc', 'src/bdc/bdc-gui'] ,
      author='Adam Jenkins',
      author_email='adamjenkins1701@gmail.com',
