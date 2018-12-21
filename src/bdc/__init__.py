7d9b36b4125df41a03ee027b335d23da80d315c9
adamjenkins1 <adamjenkins1701@gmail.com>
2018-12-20 16:50:36 -0800

    Adds correct copyright field in __init__.py

--- src/bdc/__init__.py
+++ src/bdc/__init__.py
@@ -4,14 +4,14 @@
 File name: __init__.py
 Author: Adam Jenkins
 Date created: 10/30/2018
-Date last modified: 11/6/18
+Date last modified: 12/20/18
 Python Version: 3.6.5
 '''
 
 __author__ = 'Adam Jenkins'
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 __name__ = 'bdc'
 __package__ = 'bdc'
 __url__ = 'https://www.github.com/adamjenkins1/BootableDiskCreator'
-__copyright__ = 'Copyright 2007, The Cogent Project'
+__copyright__ = 'Copyright 2018, Adam Jenkins'
 __license__ = 'MIT'
